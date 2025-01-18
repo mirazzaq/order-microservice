78700d67aabd2f0c202ced2b89506b2353ca86d9
mirazzaq <mirazzaq@gmail.com>
2025-01-18 13:56:38 +0500

    configure access middleware

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::group(['prefix' => 'orders'], function(){
+Route::group(['prefix' => 'orders', 'middleware' => 'access'], function(){
     Route::get('/', [OrderController::class, 'index']);
     Route::post('/', [OrderController::class, 'store']);
     Route::get('/{order}', [OrderController::class, 'show']);
